873d6ceae688b78be8f9775e0306d2419a87285f
weixy17 <59783182+weixy17@users.noreply.github.com>
2025-08-25 15:28:59 +0800

    Update main_siftflowmap.m

--- ACROBAT/DenseSIFTmap/main_siftflowmap.m
+++ ACROBAT/DenseSIFTmap/main_siftflowmap.m
@@ -7,9 +7,6 @@ for i=3:size(files,1)
     filename=files(i).name;
     im1=imread(fullfile(datapath,filename));
     im1=imfilter(im1,fspecial('gaussian',7,1.),'same','replicate');
-    %%%%%%%%resize to different scale
-    im1=imresize(im1,1/2,'bicubic');
-    %%%%%%%%resize to different scale
     im1=im2double(im1);
     cellsize=3;
     gridspacing=1;
